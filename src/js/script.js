249d593aae5a1c8cb537338db5cbe1ecfbfbbbf7
diegvn7 <diegovn7@gmail.com>
2025-04-21 10:39:59 -0300

    Update script.js

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+window.document.getElementById('open_btn').addEventListener('click', () =>{
+    console.log('oi')
+    window.document.getElementById('sidebar').classList.toggle('open-sidebar')
+})
